c33b9a5ba1b30670800352ff74509c59c9c526a0
lastuchiha <107856521+lastuchiha@users.noreply.github.com>
2022-07-05 22:22:31 +0530

    Create Exp3c.py

---
+++ Exp3c.py
@@ -0,0 +1,15 @@
+# Q) Loop over a sequence by using a for loop.
+
+list = [1, 2, 3, 4, 5]
+
+sum = 0
+for i in range(len(list)):
+	sum += list[i]
+print(sum)
+
+'''
+Sample Input:
+
+Sample Output:
+15
+'''
