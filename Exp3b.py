b93ac14c17776e6d75c99e7784a20528848f5cff
lastuchiha <107856521+lastuchiha@users.noreply.github.com>
2022-07-05 22:18:47 +0530

    Create Exp3b.py

---
+++ Exp3b.py
@@ -0,0 +1,16 @@
+#Print decimal Equivalents of 1/2, 1/3, 1/4 ... 1/10
+
+n = int(input("Enter a number: "))
+for i in range(2, n+1):
+  print(1/i)
+  
+'''
+Sample Input:
+Enter a number: 3
+
+Sample Output:
+0.5
+0.333333
+
+'''
+       
