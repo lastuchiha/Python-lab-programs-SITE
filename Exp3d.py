f691726b07b8d4563567e800543e5661267c4fa3
lastuchiha <107856521+lastuchiha@users.noreply.github.com>
2022-07-05 22:25:45 +0530

    Create Exp3d.py

---
+++ Exp3d.py
@@ -0,0 +1,19 @@
+# Q) Print countdown from a number to zero.
+
+n = int(input("Enter a number: "))
+while n > -1:
+	print(n)
+	n -= 1
+	
+'''
+Sample Input:
+Enter a number: 5
+
+Sample Output:
+5
+4
+3
+2
+1
+0
+'''
