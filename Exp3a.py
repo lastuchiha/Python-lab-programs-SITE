a2a964430fb9603687aade9153bcf41d64297390
lastuchiha <107856521+lastuchiha@users.noreply.github.com>
2022-07-05 22:15:05 +0530

    Create Exp3a.py

---
+++ Exp3a.py
@@ -0,0 +1,15 @@
+#Q) Check whether the given number is Even or Odd.
+
+n = int(input("Enter a number: "))
+if n%2:
+  print(n,"is an Odd number")
+else:
+  print(n,"is an Even number")
+  
+'''
+Sample Input:
+Enter a number: 5
+
+Sample Output:
+5 is an Odd number
+'''
